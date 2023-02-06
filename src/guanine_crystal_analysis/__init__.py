563ec7b758b87994351cb870a47de7b86f253b1c
Mara Lampert <97048948+marabuuu@users.noreply.github.com>
2023-02-06 13:30:49 +0100

    Update __init__.py

--- src/guanine_crystal_analysis/__init__.py
+++ src/guanine_crystal_analysis/__init__.py
@@ -1,5 +1,12 @@
 __version__ = "0.0.1"
 from ._sample_data import make_sample_data
+from ._widget import normalization
+from ._widget import segmentation
+from ._widget import analyze_image
+from ._widget import classify_objects
+from ._widget import classifier
+from ._widget import bad_label_exclusion
+from ._widget import analyze_deluxe
 
 __all__ = (
     "make_sample_data"
